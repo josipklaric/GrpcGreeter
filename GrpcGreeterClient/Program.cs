18503c08a6bf423d6bb1330dcfc549910d6cf07f
Josip Klaric <josip.klaric@infobip.com>
2020-05-08 13:58:20 +0200

    Add project files.

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        const string url = "https://localhost:5001";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("+".PadRight(50, '+'));
+            Console.WriteLine("+++ Grpc client sample +++");
+            Console.WriteLine("+".PadRight(50, '+'));
+
+            Console.WriteLine($"Instatntiating gRPC channel to {url}");
+
+            using var channel = GrpcChannel.ForAddress(url);
+            var client = new Greeter.GreeterClient(channel);
+            Console.WriteLine($"Sending new request to SayHallo ...");
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "JosipK" });
+            Console.WriteLine($"Reply from server: " + reply.Message);
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
