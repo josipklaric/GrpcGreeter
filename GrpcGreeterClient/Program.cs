3fc52b58244f2c16e8724f5d936d7a794a4588f7
Josip Klaric <josip.klaric@infobip.com>
2020-05-10 14:15:50 +0200

    Moved proto files to separate folder shared among both projects. Also output of generated grpc classes redirected to Models folder. Added try/catch for client call.

--- GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/Program.cs
@@ -16,13 +16,20 @@ namespace GrpcGreeterClient
             Console.WriteLine("+++ Grpc client sample +++");
             Console.WriteLine("+".PadRight(50, '+'));
 
-            Console.WriteLine($"Instatntiating gRPC channel to {url}");
+            Console.WriteLine($"Instantiating gRPC channel to {url}");
 
             using var channel = GrpcChannel.ForAddress(url);
             var client = new Greeter.GreeterClient(channel);
-            Console.WriteLine($"Sending new request to SayHallo ...");
-            var reply = await client.SayHelloAsync(new HelloRequest { Name = "JosipK" });
-            Console.WriteLine($"Reply from server: " + reply.Message);
+            Console.WriteLine($"Sending new request to SayHallo method of Greet service ...");
+            try
+            {
+                var reply = await client.SayHelloAsync(new HelloRequest { Name = "JosipK" });
+                Console.WriteLine($"Reply from server: " + reply.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error occurred while calling Grret service! " + ex.Message);
+            }
 
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
